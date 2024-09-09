73a78b0c6ab1e1cd528c7aa2da3dffa59128675d
Alexander Nefedov <nefedov.a.v@ft-soft.ru>
2024-09-09 21:29:26 +0500

    Перенесен тест checkIsSpinNotLegal в пакет tests

--- tests/handlers_helper_test.go
+++ tests/handlers_helper_test.go
@@ -1,32 +1,36 @@
-package handlers
+package tests
 
 import (
 	"testing"
 	"time"
+	_ "unsafe"
 )
 
-func TestCheckIsSpinNotLegal(t *testing.T) {
+//go:linkname handlersCheckIsSpinNotLegal github.com/denis1011101/super_cum_bot/app/handlers.checkIsSpinNotLegal
+func handlersCheckIsSpinNotLegal(lastUpdate time.Time) bool
+
+func TestCheckIsSpinNotLegal(t *testing.T) {	
 	// Test case: Last update is today and duration is less than 4 hours
 	lastUpdate := time.Now().Add(-1 * time.Hour)
-	if checkIsSpinNotLegal(lastUpdate) != true {
+	if handlersCheckIsSpinNotLegal(lastUpdate) == false {
 		t.Errorf("Expected true, but got false")
 	}
 
 	// Test case: Last update is today and equeals
 	lastUpdate = time.Now().Add(time.Hour)
-	if checkIsSpinNotLegal(lastUpdate) != true {
+	if handlersCheckIsSpinNotLegal(lastUpdate) == false {
 		t.Errorf("Expected true, but got false")
 	}
 
 	// Test case: Last update is not today
 	lastUpdate = time.Now().Add(-24 * time.Hour)
-	if checkIsSpinNotLegal(lastUpdate) != false {
+	if handlersCheckIsSpinNotLegal(lastUpdate) == true {
 		t.Errorf("Expected false, but got true")
 	}
 
 	// Test case: Last update is zero
 	lastUpdate = time.Time{}
-	if checkIsSpinNotLegal(lastUpdate) != false {
+	if handlersCheckIsSpinNotLegal(lastUpdate) == true {
 		t.Errorf("Expected false, but got true")
 	}
 }
